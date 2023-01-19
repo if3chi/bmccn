99f6f1719122ac2987ac449f42b36dea583dd4ac
Truth <j.cyxtus@gmail.com>
2023-01-19 16:26:51 +0000

    Updated workshop

--- resources/views/front/workshop.blade.php
+++ resources/views/front/workshop.blade.php
@@ -13,17 +13,18 @@
     <section class="block py-24 bg-white lg:pt-0 fade-in">
         <div class="container px-4 pt-6 mx-auto lg:pt-12">
             <div class="flex flex-wrap items-center mt-24">
-                <x-img title="Accra New Town Experimental School, Accra." src="{{ setSchoolImg() }}" alt="Photo with school children" />
-                <x-img title="Accra New Town Experimental School, Accra." src="{{ setSchoolImg('2') }}" alt="Photo with school children" />
-                <x-img title="Gudmerc Academy, Abuja." src="{{ setSchoolImg('z') }}" alt="Photo with school children" />
-                <x-img title="Accra New Town Experimental School, Accra." src="{{ setSchoolImg('3') }}" alt="Photo with school children" />
-                <x-img title="Gracewell Academy Surulere Lagos." src="{{ setSchoolImg('a') }}" alt="Photo with school children" />
-                <x-img title="Gracewell Academy Surulere Lagos." src="{{ setSchoolImg('b') }}" alt="Photo with school children" />
-                <x-img title="Gudmerc Academy, Abuja." src="{{ setSchoolImg('gudmerc') }}" alt="Photo with school children" />
-                <x-img title="KARU Government Cecondary School Karu Abuja" src="{{ setSchoolImg('karu') }}" alt="Photo with school children" />
-                <x-img title="KARU Government Cecondary School Karu Abuja" src="{{ setSchoolImg('karu1') }}" alt="Photo with school children" />
-                <x-img title="KARU Government Cecondary School Karu Abuja" src="{{ setSchoolImg('karu2') }}" alt="Photo with school children" />
-                <x-img title="KARU Government Cecondary School Karu Abuja" src="{{ setSchoolImg('karu3') }}" alt="Photo with school children" />
+                <x-img title="Accra New Town Experimental School, Accra, Ghana." src="{{ setSchoolImg() }}" alt="Photo with school children" />
+                <x-img title="Accra New Town Experimental School, Accra, Ghana." src="{{ setSchoolImg('2') }}" alt="Photo with school children" />
+                <x-img title="Accra New Town Experimental School, Accra, Ghana." src="{{ setSchoolImg('3') }}" alt="Photo with school children" />
+                <x-img title="Gracewell Academy Surulere, Lagos, Nigeria." src="{{ setSchoolImg('a') }}" alt="Photo with school children" />
+                <x-img title="Gracewell Academy Surulere, Lagos, Nigeria." src="{{ setSchoolImg('b') }}" alt="Photo with school children" />
+                <x-img title="Gracewell Academy Surulere, Lagos, Nigeria." src="{{ setSchoolImg('c') }}" alt="Photo with school children" />
+                <x-img title="Gudmerc Academy, Abuja, Nigeria." src="{{ setSchoolImg('gudmerc2') }}" alt="Photo with school children" />
+                <x-img title="Gudmerc Academy, Abuja, Nigeria." src="{{ setSchoolImg('gudmerc1') }}" alt="Photo with school children" />
+                <x-img title="Gudmerc Academy, Abuja, Nigeria." src="{{ setSchoolImg('gudmerc') }}" alt="Photo with school children" />
+                <x-img title="KARU Government Secondary School, Karu Abuja, Nigeria." src="{{ setSchoolImg('karu2') }}" alt="Photo with school children" />
+                <x-img title="KARU Government Secondary School, Karu Abuja, Nigeria." src="{{ setSchoolImg('karu') }}" alt="Photo with school children" />
+                <x-img title="KARU Government Secondary School, Karu Abuja, Nigeria." src="{{ setSchoolImg('karu1') }}" alt="Photo with school children" />
             </div>
         </div>
     </section>
