c432d66754232ba6affa38215428f0e536b41c52
Truth <j.cyxtus@gmail.com>
2023-01-21 02:51:32 +0000

    WIP art gallery

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/art-gallery', 'front.art-gallery')->name('art-gallery');
 Route::view('/creative-writing', 'front.creative-writing')->name('creative-writing');
 Route::view('/kids2action', 'front.kids2action')->name('kids2action');
 Route::view('/leardership-workshop', 'front.workshop')->name('workshop');
